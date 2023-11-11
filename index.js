f96aeb49db6379b6510fd5f5196e5b760e8635f0
AshongAbdallah06 <ashongabdalla15@gmail.com>
2023-11-11 18:53:06 +0000

    Make share button interactive

---
+++ index.js
@@ -0,0 +1,6 @@
+let shareButton = document.querySelector('.js-share-btn');
+shareButton.addEventListener('click', () => {
+
+    document.querySelector('.share-options').classList.add('Share-Options')
+})
+
