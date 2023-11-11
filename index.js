1de0c2fd302538784a9b0aedd2af50b80ae249de
AshongAbdallah06 <ashongabdalla15@gmail.com>
2023-11-11 21:57:32 +0000

    Change share icon color

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ shareButton.addEventListener('click', () => {
     const shareOptions = document.querySelector('.share-options');
 
     if (!shareOptions.classList.contains('is-sharing')) {
-        document.querySelector('.share-options').classList.add('is-sharing')
+        document.querySelector('.share-options').classList.add('is-sharing');
+        document.querySelector('.share-btn').classList.add('Share-btn');
+        document.querySelector('.share-svg').classList.add('share-icon')
+
     } else {
-        document.querySelector('.share-options').classList.remove('is-sharing')
+        document.querySelector('.share-options').classList.remove('is-sharing');
+        document.querySelector('.share-btn').classList.remove('Share-btn');
+        document.querySelector('.share-svg').classList.remove('share-icon')
 
     }
-    
+
 })
 
