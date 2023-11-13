982c71dc13c08dcd0fbc91e70b11088f872ebdd1
AshongAbdallah06 <ashongabdalla15@gmail.com>
2023-11-13 14:48:56 +0000

    Move CSS and JavaScript into new folders

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,35 @@
 const shareButton = document.querySelector('.js-share-btn');
 
-let main = document.getElementById('main');
-
-// const shareButton = document.querySelector('.share-button');
-const shareOptionsColumn = document.querySelector('.share-options-column');
 
 function checkStyles() {
-  const width = window.innerWidth;
-  const computedStyle = window.getComputedStyle(main);
-  const flexDirection = computedStyle.getPropertyValue('flex-direction');
+    let main = document.getElementById('main');
+    const width = window.innerWidth;
+    const computedStyle = window.getComputedStyle(main);
+    const flexDirection = computedStyle.getPropertyValue('flex-direction');
 
-  if (width <= 700 && flexDirection === 'column') {
-    // Your code here for when the page width is 700 pixels or less and main is in column direction
+    if (width <= 700 && flexDirection === 'column') {
+        // Your code here for when the page width is 700 pixels or less and main is in column direction
 
-    document.querySelector('.mobile-share').innerHTML = `
-    <span class="share-options-column">
-        <span class="container">
-            <span class="tooltip">SHARE</span>
-            <img src="images/icon-facebook.svg" class="icons-column" alt="facebook icon">
-            <img src="images/icon-twitter.svg" class="icons-column" alt="twitter-icon">
-            <img src="images/icon-pinterest.svg" class="icons-column" alt="pinterest-icon">
-        </span>
+        document.querySelector('.mobile-share').innerHTML = `
+        <span class="share-options-column">
+            <span class="container">
+                <span class="tooltip">SHARE</span>
+                <img src="images/icon-facebook.svg" class="icons-column" alt="facebook icon">
+                <img src="images/icon-twitter.svg" class="icons-column" alt="twitter-icon">
+                <img src="images/icon-pinterest.svg" class="icons-column" alt="pinterest-icon">
+            </span>
 
-        <img class="mobile-share-svg" src="images/icon-share.svg" alt="">
+            <img class="mobile-share-svg" src="images/icon-share.svg" alt="">
 
-    </span>`
-    
-    document.querySelector('.share-options-column').classList.add('is-sharing-options')
+        </span>`
+        
+        document.querySelector('.share-options-column').classList.add('is-sharing-options')
 
-    document.querySelector('.user-container').classList.add('remove-user-container')
-  } else {
-    // Your code here for other cases
+        document.querySelector('.user-container').classList.add('remove-user-container')
+    } else {
+        // Your code here for other cases
 
-    document.querySelector('.user-container').classList.remove('remove-user-container');
+        document.querySelector('.user-container').classList.remove('remove-user-container');
 
 
 
@@ -54,31 +51,27 @@ function checkStyles() {
 
         document.querySelector('.share-svg').classList.remove("share-icon")
         document.querySelector('.js-share-btn').classList.remove("Share-btn");
-
-
     }
-
-
   }
 }
 
-// Initial check on page load
-// checkStyles();
+
 
 // Check styles when the window is resized
 window.addEventListener('resize', () => {
     const width = window.innerWidth;
-    const computedStyle = window.getComputedStyle(main);
-    const flexDirection = computedStyle.getPropertyValue('flex-direction');
   
     if (width > 700) {
         document.querySelector('.user-container').classList.remove('remove-user-container')
     } else {
-        document.querySelector('.user-container').classList.add('remove-user-container')
+        if (!document.querySelector('.share-options-column').classList.contains('mobile-svg')) {
+            document.querySelector('.user-container').classList.add('remove-user-container')
+        }
 
     }
 
 });
+
 // Check styles when the button is clicked
 shareButton.addEventListener('click', () => {
     checkStyles();
